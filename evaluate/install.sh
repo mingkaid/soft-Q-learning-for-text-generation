4adb79f87be858945bca666dc0fe5e60c0cc89cb
hsiehjackson <c2hsieh@ucsd.edu>
2022-01-04 11:40:22 -0800

    add large file

--- evaluate/install.sh
+++ evaluate/install.sh
@@ -4,8 +4,7 @@ wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1C2b3B-XzO7kkryUmbwmM9ute0RJoiTxw' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1C2b3B-XzO7kkryUmbwmM9ute0RJoiTxw" -O style.tar.gz && rm -rf /tmp/cookies.txt
 
-
-# tar -zxvf dataset.tar.gz
-# tar -zxvf ppl.tar.gz
-# tar -zxvf style.tar.gz
-# pip install -r requirements.txt
+tar -zxvf dataset.tar.gz
+tar -zxvf ppl.tar.gz
+tar -zxvf style.tar.gz
+pip install -r requirements.txt
